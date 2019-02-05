f2c3ed8bd11530b0055a4d40d384192e8beb1492
xi <xisun1990@gmail.com>
2019-02-05 00:31:12 -0800

    import styled-component

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <Welcome/>
+        <div>
+            <Welcome/>
+        </div>
     );
   }
 }
