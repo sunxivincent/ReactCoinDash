50e3c69f9eebe914260fd1726ead58712d4a04a9
xi <xisun1990@gmail.com>
2019-02-05 01:06:01 -0800

    add app layout using styled-component

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Welcome from './WelcomeMessage';
 import styled, {css} from 'styled-components';
+import AppLayout from './AppLayout';
 
 class App extends Component {
   render() {
     return (
-        <div>
+        <AppLayout>
             <Welcome/>
-        </div>
+        </AppLayout>
     );
   }
 }
