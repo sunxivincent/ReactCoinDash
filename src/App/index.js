28d69582dfc1ba62c9a2614a0bf3de014fa8e351
xi <xisun1990@gmail.com>
2019-02-06 22:29:31 -0800

    add app bar

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,14 @@ import './App.css';
 import Welcome from './WelcomeMessage';
 import styled, {css} from 'styled-components';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+
 
 class App extends Component {
   render() {
     return (
         <AppLayout>
+            <AppBar/>
             <Welcome/>
         </AppLayout>
     );
