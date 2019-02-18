d07087142d773c3a9401d6d2bda7ee0c11267b07
xi <xisun1990@gmail.com>
2019-02-18 02:29:48 -0800

    learn how to use local storage

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -5,19 +5,19 @@ import {AppContext} from "./AppProvider";
 const Bar = styled.div `
     display: grid;
     grid-template-columns: 180px auto 180px 180px;
-    margin-botton: 60px;
-`
+    margin-bottom: 60px;
+`;
 
 const Logo = styled.div `
     font-size: 1.5em;
-`
+`;
 
 const ControlButtonElement = styled.div`
     cursor: pointer;
     ${props => props.active && css`
         text-shadow: 0px 0px 30px #03ff03
     `};
-`
+`;
 
 function ControlButton({name}) {
   return (
@@ -40,7 +40,7 @@ export default function () {
       <Logo>Crypto Dash</Logo>
       <div/>
       <ControlButton name="Dashboard"/>
-      <ControlButton name="Setting"/>
+      <ControlButton name="Settings"/>
     </Bar>
   );
 }
