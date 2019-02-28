b10a4572e11f32e670655f6071531aafaec9ac56
xi <xisun1990@gmail.com>
2019-02-28 01:36:27 -0800

    add coin grid style

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Welcome from './WelcomeMessage';
 import ConfirmButton from './ConfirmedButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function () {
   return (
     <Page name="Settings">
       <Welcome/>
       <ConfirmButton/>
+      <CoinGrid/>
     </Page>
   )
 }
