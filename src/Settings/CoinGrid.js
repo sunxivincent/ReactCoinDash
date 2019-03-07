f9b6dd54711d09099a7ada441652fa4a9670964e
xi <xisun1990@gmail.com>
2019-03-06 17:53:43 -0800

    create shared tile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from "../App/AppProvider";
-
+import {SelectableTile} from "../Shared/Tile";
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5,1fr)
+  grid-template-columns: repeat(5,1fr);
+  grid-gap: 14px;
 `;
 
 export default function () {
   return <AppContext.Consumer>
     {({coinList}) =>
       <CoinGridStyled>
-        {Object.keys(coinList).map(coin => <div> {coin} </div>)}
+        {Object.keys(coinList).map(coin => <SelectableTile> {coin} </SelectableTile>)}
       </CoinGridStyled>}
   </AppContext.Consumer>
 }
--- src/Settings/ConfirmedButton.js
+++ src/Settings/ConfirmedButton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AppContext} from "../App/AppProvider";
+import {fontSize1, greenBoxShadow, color3} from "../Shared/Styles";
 
 const ConfirmButtonStyled = styled.div `
   margin: 20px;
-  color: green;
+  color: ${color3};
   cursor: pointer;
+  {$fontSize1};
+  padding: 5px;
+  &:hover {
+   ${greenBoxShadow}
+  }
 `;
 
 export const CenterDiv = styled.div `
